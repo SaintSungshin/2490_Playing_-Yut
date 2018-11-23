5b4683cea07b97b7e0e29ea794aa4c8237d99bb1
kkmjkim <44939907+kkmjkim@users.noreply.github.com>
2018-11-23 18:28:50 +0900

    Add files via upload

---
+++ MJ_2490_Playing_Yut.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	//Create an array list
+	const int ROW = 3;
+	const int COL = 4;
+	int list[ROW][COL];
+
+	//Enter numbers
+	for (int i = 0; i < ROW; i++) {
+		
+		for (int j = 0; j < COL; j++)
+			cin >> list[i][j];
+		
+		cout << endl;
+	}
+
+	int count = 0;
+
+	for (int m = 0; m < ROW; m++) {
+		
+		for (int n = 0; n < COL; n++) {
+			if (list[m][n] == 0)
+				count++;
+		}
+		
+		//Count numbers of front
+		switch (count) {
+			case 0: cout << 'E' << endl; break;
+			case 1: cout << 'A' << endl; break;
+			case 2: cout << 'B' << endl; break;
+			case 3: cout << 'C' << endl; break;
+			case 4: cout << 'D' << endl; break;
+		}
+		
+		count = 0;
+		cout << endl;
+	}
+
+	system("pause");
+	return 0;
+}
