d7558538cd2b859be1d7b4a6cc3297b3001e3c55
JayKimm <44940633+JayKimm@users.noreply.github.com>
2018-11-26 16:03:45 +0900

    Update JayKim_2490_Yut.cpp

--- JayKim_2490_Yut.cpp
+++ JayKim_2490_Yut.cpp
@@ -6,7 +6,8 @@ int main()
 	cout << "Enter three set of numbers. : " << endl;
 	int set[3][4];
 	int result[3];
-	for (int i = 0;i < 3;i++)
+	
+	for (int i = 0;i < 3;i++)// 사용자 숫자 배열에 저장하긩
 	{
 		int total = 0;
 
@@ -15,9 +16,9 @@ int main()
 			cin >> set[i][j];
 			total += set[i][j];
 		}
-		result[i] = total;
+		result[i] = total;// 세트마다의 토탈을 따로 저장
 	}
-	for (int i = 0;i < 3;i++)
+	for (int i = 0;i < 3;i++)// 값에 따라 출력!
 	{
 		if (result[i] == 0)
 			cout << "D" << endl;
